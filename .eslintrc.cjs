2faa18144f612f54a44cd863ef65443f990e174e
Arterixs <artemontugg@gmail.com>
2023-07-06 11:43:10 +0300

    chore: add setting enviroment develop

---
+++ .eslintrc.cjs
@@ -0,0 +1,43 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+    'airbnb-typescript/base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'prettier',
+  ],
+  overrides: [
+    {
+      files: ['*.ts', '.eslintrc.{cjs}'],
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+      ],
+      env: {
+        node: true,
+      },
+      parserOptions: {
+        sourceType: 'script',
+      },
+      rules: {
+        'import/no-default-export': 'off',
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json'],
+  },
+  plugins: ['@typescript-eslint', 'import', 'prettier'],
+  rules: {
+    'import/extensions': [1, { ts: 'always' }],
+    'import/prefer-default-export': 'off',
+    'import/no-unresolved': 'off',
+  },
+};
